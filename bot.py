61f2f03db2b635fa8c5fd22ab3897fbcf4d0037e
Andrea Zanin <andrea@igloo.ooo>
2021-10-25 11:39:27 +0200

    hello message

--- bot.py
+++ bot.py
@@ -3,13 +3,16 @@ import telebot
 # Setup del bot
 bot = telebot.TeleBot('2095077251:AAHSBgqqsjG23YigKDT43o-18BhDjiEj1SM')
 
+
 @bot.message_handler(commands=['hello'])
 def hello(message):
-  bot.send_message(message.chat.id, "Hello!")
+    bot.send_message(message.chat.id, "Hello!")
+
 
 @bot.message_handler(commands=['start'])
 def prova(message):
-  bot.send_message(message.chat.id, "Hello! \n sus")
-  bot.send_message(message.chat.id, "bye")
+    bot.send_message(
+        message.chat.id, "Ciao, sono il bot del dipartimento di Matematica di UniTN.\nAl momento sono ancora in sviluppo.")
+
 
 bot.polling()
